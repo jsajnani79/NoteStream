b4396fe484be2451dd758b59da9c713eb6e083cb
Yifan Wu <yifan1030@gmail.com>
2014-07-26 13:10:40 -0700

    added collections

---
+++ collections/notes.js
@@ -0,0 +1,42 @@
+Notes = new Meteor.Collection("reviews", {
+    schema: {
+        role: {
+            type: String,
+            label: "teacher, or student",
+        },
+        documentId: {
+            type: String,
+            label: "Associate docuemnt ID"
+        },
+        createdBy: {
+          type: String,
+          label: "Created By UserID"
+        },
+        createdAt: {
+          type: Date,
+          label: "Created at Time"
+        },
+        sharedWith: {
+          type: [String],
+          label: "Shared with what users"
+        }
+    }
+});
+
+Notes.allow({
+  insert: function(userId, doc) {
+    // only allow posting if you are logged in
+    return !! userId;
+  },
+  update: function(userId, doc) {
+    if (userId === doc.createdBy)
+      return !! userId;
+    else return false;
+  },
+  remove: function(userId, doc) {
+    if (doc.createdBy == userId)
+      return true;
+    else return false;
+  }
+});
+
