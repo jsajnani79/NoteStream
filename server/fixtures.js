8a82d768c005de514f612d303fe0c6d8552cff23
Anna Swigart <annagswigart@gmail.com>
2014-07-27 01:22:09 -0700

    toggle works

--- server/fixtures.js
+++ server/fixtures.js
@@ -20,7 +20,7 @@ if (Notes.find().count() === 0) {
 		createdBy: "tim",
 		// createdAt: ,
 		sharedWith: ["joe", "sue", "kelly"],
-		textNote: "This is yet another note", 
+		textNote: "Irony cliche keffiyeh disrupt ugh banjo bicycle rights, hoodie +1 quinoa. Cornhole XOXO drinking vinegar lo-fi semiotics ennui, cray mlkshk lomo. Brooklyn Shoreditch single-origin coffee, squid master cleanse tousled Bushwick literally Neutra. Fap twee four loko, keffiyeh stumptown Odd Future Blue Bottle ethnic Tonx skateboard kale chips. Meh cornhole art party, Brooklyn banh mi Shoreditch stumptown Blue Bottle viral. Brunch hella direct trade fanny pack Tonx farm-to-table, Austin typewriter. Ennui High Life pop-up photo booth crucifix banjo, fixie single-origin coffee before they sold out retro umami paleo.", 
 	});
 
 }
